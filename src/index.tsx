41ee5881d8f51ecdfc42c7a1c502c675d158aa79
miyazaki <kattyan.08ms@gmail.com>
2020-08-23 03:19:01 +0900

    index.tsxにbootstrapのcssを入れた

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
